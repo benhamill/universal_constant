5c896773279946f76d766755ddee937fbf35a66e
Ben Hamill <git-commits@benhamill.com>
2011-09-19 00:23:37 -0500

    UI#say.

---
+++ lib/universal_constant/ui.rb
@@ -0,0 +1,7 @@
+module UniversalConstant
+  module UI
+    def self.say(stuff)
+      puts(stuff.to_s)
+    end
+  end
+end
---
+++ spec/ui.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe UniversalConstant::UI do
+  describe "#say" do
+    it "prints out the string representation of what's passed in" do
+      subject.should_receive(:puts).with('It is very dark.')
+      subject.say('It is very dark.')
+    end
+  end
+end
