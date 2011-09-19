213774413e81703ac3938c7df2eaebefd1eee701
Ben Hamill <git-commits@benhamill.com>
2011-09-19 01:45:25 -0500

    Ask command.

--- lib/universal_constant/game/ui.rb
+++ lib/universal_constant/game/ui.rb
@@ -7,6 +7,18 @@ module UniversalConstant
         output_stream.puts(stuff.to_s)
       end
 
+      def self.list(*items)
+        items.each_with_index do |item, index|
+          say("#{index + 1}. #{item.to_s}")
+        end
+      end
+
+      def self.ask(message = nil)
+        say(message) if message
+        @@output_stream.print('> ')
+        gets
+      end
+
       private
 
       def self.output_stream
--- spec/game/ui.rb
+++ spec/game/ui.rb
@@ -11,7 +11,36 @@ describe UniversalConstant::Game::UI do
   describe "#say" do
     it "prints out the string representation of what's passed in" do
       subject.say('It is very dark.')
+
       output.should == "It is very dark.\n"
+    end
+  end
+
+  describe "#list" do
+    it "lists each string in passed in, numbered" do
+      subject.list('First item', 'Second item', 'Third item')
+
+      output.should == <<-OUT
+1. First item
+2. Second item
+3. Third item
+OUT
+    end
+  end
+
+  describe "#ask" do
+    it "displays a message and returns user input" do
+      subject.stub(:gets) { 'Nada mucho.' }
+
+      subject.ask("What's up?").should == 'Nada mucho.'
+      output.should == "What's up?\n> "
+    end
+
+    it "just displays the prompt with no message" do
+      subject.stub(:gets) { 'look' }
+
+      subject.ask.should == 'look'
+      output.should == "> "
     end
   end
 end
