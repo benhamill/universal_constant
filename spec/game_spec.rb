cabe0b69982673fd1cd6f35e38c9b015888c5a5e
Ben Hamill <git-commits@benhamill.com>
2011-09-22 23:09:32 -0500

    Refactor tests.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,18 +3,15 @@ require 'spec_helper'
 describe UniversalConstant::Game do
   describe ".new" do
     it "creates a new Player object" do
-      UniversalConstant::Player.stub!(:new) { :player }
-
-      UniversalConstant::Game.new.player.should == :player
+      UniversalConstant::Game.new.player.should be_a(UniversalConstant::Player)
     end
   end
 
   describe "#player_location" do
     it "returns the player's current location" do
-      player = double('Player', :location => :at_home)
-      UniversalConstant::Player.stub!(:new) { player }
+      subject.player.location = :at_home
 
-      UniversalConstant::Game.new.player_location.should == :at_home
+      subject.player_location.should == :at_home
     end
   end
 end
