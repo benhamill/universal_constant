c47c6b1cfdbfa4bd2c1e6de1f446595d7b4d8218
Ben Hamill <git-commits@benhamill.com>
2011-09-22 22:39:48 -0500

    Basic game/player interface.

--- lib/universal_constant/game.rb
+++ lib/universal_constant/game.rb
@@ -2,4 +2,14 @@ require_relative 'game/ui'
 
 class UniversalConstant::Game
   include UI
+
+  attr_reader :player
+
+  def initialize
+    @player = UniversalConstant::Player.new
+  end
+
+  def player_location
+    @player.location
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 
 describe UniversalConstant::Game do
+  describe ".new" do
+    it "creates a new Player object" do
+      UniversalConstant::Player.stub!(:new) { :player }
+
+      UniversalConstant::Game.new.player.should == :player
+    end
+  end
+
+  describe "#player_location" do
+    it "returns the player's current location" do
+      player = double('Player', :location => :at_home)
+      UniversalConstant::Player.stub!(:new) { player }
+
+      UniversalConstant::Game.new.player_location.should == :at_home
+    end
+  end
 end
