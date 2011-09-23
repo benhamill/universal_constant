1808154c24752e9ab7cffbc918abadc75b13b081
Ben Hamill <git-commits@benhamill.com>
2011-09-22 23:36:07 -0500

    Refactor tests.

--- spec/game_object_spec.rb
+++ spec/game_object_spec.rb
@@ -3,20 +3,23 @@ require 'spec_helper'
 describe UniversalConstant::GameObject do
   describe ".find" do
     it "gets an object with the given id" do
+      clear_object_cache
       5.times { UniversalConstant::GameObject.new }
       g = UniversalConstant::GameObject.new
       5.times { UniversalConstant::GameObject.new }
 
-      UniversalConstant::GameObject.find(g.id).should === g
+      UniversalConstant::GameObject.find(5).should === g
     end
   end
 
   describe ".new" do
     it "assigns a sequential id to each new object" do
+      clear_object_cache
       first_id = UniversalConstant::GameObject.new.id
       second_id = UniversalConstant::GameObject.new.id
 
-      first_id.should ==  second_id - 1
+      first_id.should == 0
+      second_id.should == 1
     end
 
     it "assigns hash values to instance variables matching the hash keys" do
