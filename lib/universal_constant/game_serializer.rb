15a53bf56f3b6bf2b73c9ac06e8022183fd72f73
Ben Hamill <git-commits@benhamill.com>
2011-09-24 23:01:58 -0500

    One more step.

--- lib/universal_constant/game_serializer.rb
+++ lib/universal_constant/game_serializer.rb
@@ -10,5 +10,14 @@ module UniversalConstant
     def create_save_root
       FileUtils.mkdir(save_root)
     end
+
+    def write_game_data
+      File.open(File.join(save_root, 'game'), 'w') do |file|
+        file.puts YAML.dump({
+          'name' => game.name,
+          'start_id' => game.starting_location.id,
+        })
+      end
+    end
   end
 end
--- spec/game_serializer_spec.rb
+++ spec/game_serializer_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe UniversalConstant::GameSerializer do
   before(:each) do
     clear_object_cache
-    @game = UniversalConstant::Game.new(:name => 'The Game', :start_id => 0)
     @top = UniversalConstant::Room.new(:description => 'Up high', :name => 'Top')
     @bottom = @top.dig('Down', 'Bottom', :back => 'Up')
     @bottom.description = 'Down low.'
+    @game = UniversalConstant::Game.new(:name => 'The Game', :start_id => 0)
 
     @serializer = UniversalConstant::GameSerializer.new(@game, SPEC_WORKING_DIR)
   end
@@ -25,6 +25,23 @@ describe UniversalConstant::GameSerializer do
     it "creates a directory named after the game" do
       @serializer.create_save_root
       Dir.exists?(@serializer.save_root).should be_true
+    end
+  end
+
+  describe "#write_game_data" do
+    before(:each) do
+      @serializer.write_game_data
+    end
+
+    it "creates a file called game in the save root" do
+      File.exists?(File.join(@serializer.save_root, 'game'))
+    end
+
+    it "stores high-level game data to the file" do
+      File.read(File.join(@serializer.save_root, 'game')).should == YAML.dump({
+        'name' => @game.name,
+        'start_id' => 0,
+      })
     end
   end
 end
