3c6cc63121b96e3b655ab4864bc7597a56739ce9
Ben Hamill <git-commits@benhamill.com>
2011-09-24 23:09:02 -0500

    This order makes more sense to me.

--- spec/game_serializer_spec.rb
+++ spec/game_serializer_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe UniversalConstant::GameSerializer do
   before(:each) do
     clear_object_cache
+    @game = UniversalConstant::Game.new(:name => 'The Game', :start_id => 0)
     @top = UniversalConstant::Room.new(:description => 'Up high', :name => 'Top')
     @bottom = @top.dig('Down', 'Bottom', :back => 'Up')
     @bottom.description = 'Down low.'
-    @game = UniversalConstant::Game.new(:name => 'The Game', :start_id => 0)
 
     @serializer = UniversalConstant::GameSerializer.new(@game, SPEC_WORKING_DIR)
   end
