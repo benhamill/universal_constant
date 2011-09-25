142e5f37bc9ebd913cb6cc4d43e297b5f32c9709
Ben Hamill <git-commits@benhamill.com>
2011-09-24 22:28:34 -0500

    One more step.

--- lib/universal_constant/game_serializer.rb
+++ lib/universal_constant/game_serializer.rb
@@ -6,5 +6,9 @@ module UniversalConstant
       @game = game
       @save_root = File.join(path, @game.name.downcase.gsub(/[^a-z0-9\-_ ]/, '').gsub(/ +/, '_'))
     end
+
+    def create_save_root
+      FileUtils.mkdir(save_root)
+    end
   end
 end
--- spec/game_serializer_spec.rb
+++ spec/game_serializer_spec.rb
@@ -1,18 +1,30 @@
 require 'spec_helper'
 
 describe UniversalConstant::GameSerializer do
-  describe ".new" do
-    before(:each) do
-      @game = UniversalConstant::Game.new(:name => 'The Game', :start_id => 0)
-      @serializer = UniversalConstant::GameSerializer.new(@game, SPEC_WORKING_DIR)
-    end
+  before(:each) do
+    clear_object_cache
+    @game = UniversalConstant::Game.new(:name => 'The Game', :start_id => 0)
+    @top = UniversalConstant::Room.new(:description => 'Up high', :name => 'Top')
+    @bottom = @top.dig('Down', 'Bottom', :back => 'Up')
+    @bottom.description = 'Down low.'
 
+    @serializer = UniversalConstant::GameSerializer.new(@game, SPEC_WORKING_DIR)
+  end
+
+  describe ".new" do
     it "stores a game" do
       @serializer.game.should === @game
     end
 
     it "calculates where everything will be saved" do
       @serializer.save_root.should == File.join(SPEC_WORKING_DIR, 'the_game')
+    end
+  end
+
+  describe "#create_save_root" do
+    it "creates a directory named after the game" do
+      @serializer.create_save_root
+      Dir.exists?(@serializer.save_root).should be_true
     end
   end
 end
